5af99707f5475fb7e41fbc6728c063d0eeb21ba6
JlFerrete <joseluisferrete@gmail.com>
2020-07-09 10:17:41 +0200

    Minor Fix

--- src/main/java/com/formacionbdi/microservicios/commons/services/CommonServiceImpl.java
+++ src/main/java/com/formacionbdi/microservicios/commons/services/CommonServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommonServiceImpl<E, R extends CrudRepository<E, Long>> implements CommonService<E> {
 
 	@Autowired
-	private R repository;
+	protected R repository;
 	
 	@Override
 	@Transactional(readOnly = true)
