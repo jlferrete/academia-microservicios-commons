60305b96befb20ef8dfec7153f2db54f1af99fe3
JlFerrete <joseluisferrete@gmail.com>
2020-07-09 08:38:59 +0200

    Initial commit

---
+++ src/main/java/com/formacionbdi/microservicios/commons/CommonsMicroserviciosApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.microservicios.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsMicroserviciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsMicroserviciosApplication.class, args);
+	}
+
+}
