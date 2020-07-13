4ff2a11798d421a8c8a7a1f4fe1b0504b2aa5b56
JlFerrete <joseluisferrete@gmail.com>
2020-07-13 13:19:41 +0200

    Adding pagination

--- src/main/java/com/formacionbdi/microservicios/commons/controllers/CommonController.java
+++ src/main/java/com/formacionbdi/microservicios/commons/controllers/CommonController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,6 +31,11 @@ public class CommonController<E, S extends CommonService<E>> {
 	@GetMapping
 	public ResponseEntity<?> listar() {
 		return ResponseEntity.ok().body(service.findAll());
+	}
+	
+	@GetMapping("/pagina")
+	public ResponseEntity<?> listar(Pageable pageable) {
+		return ResponseEntity.ok().body(service.findAll(pageable));
 	}
 	
 	@GetMapping("/{id}")
--- src/main/java/com/formacionbdi/microservicios/commons/services/CommonServiceImpl.java
+++ src/main/java/com/formacionbdi/microservicios/commons/services/CommonServiceImpl.java
@@ -3,12 +3,14 @@ package com.formacionbdi.microservicios.commons.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 
 
-public class CommonServiceImpl<E, R extends CrudRepository<E, Long>> implements CommonService<E> {
+public class CommonServiceImpl<E, R extends PagingAndSortingRepository<E, Long>> implements CommonService<E> {
 
 	@Autowired
 	protected R repository;
@@ -36,6 +38,12 @@ public class CommonServiceImpl<E, R extends CrudRepository<E, Long>> implements 
 	public void deleteById(Long id) {
 		repository.deleteById(id);
 
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Page<E> findAll(Pageable pageable) {
+		return repository.findAll(pageable);
 	}
 
 }
