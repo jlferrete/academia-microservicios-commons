7aa414399563ad15cc3fd4d622bb0ba067d119be
JlFerrete <joseluisferrete@gmail.com>
2020-07-13 12:34:03 +0200

    Adding validation rules in the entities

--- src/main/java/com/formacionbdi/microservicios/commons/controllers/CommonController.java
+++ src/main/java/com/formacionbdi/microservicios/commons/controllers/CommonController.java
@@ -1,11 +1,16 @@
 package com.formacionbdi.microservicios.commons.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.formacionbdi.microservicios.commons.services.CommonService;
 
@@ -39,8 +45,11 @@ public class CommonController<E, S extends CommonService<E>> {
 	}
 	
 	@PostMapping
-	public ResponseEntity<?> crear(@RequestBody E entity) {
+	public ResponseEntity<?> crear(@Valid @RequestBody E entity, BindingResult result) {
 		
+		if(result.hasErrors()) {
+			return this.validar(result);
+		}
 		E entityDb = service.save(entity);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(entityDb);
@@ -50,6 +59,16 @@ public class CommonController<E, S extends CommonService<E>> {
 	public ResponseEntity<?> eliminar(@PathVariable Long id){
 		service.deleteById(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	protected ResponseEntity<?> validar(BindingResult result){
+		Map<String, Object> errores = new HashMap<>();
+		result.getFieldErrors().forEach(err -> {
+			errores.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+		});
+		
+		return ResponseEntity.badRequest().body(errores);
+		
 	}
 	
 }
